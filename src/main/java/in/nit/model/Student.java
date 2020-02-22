7b51ba49cc1cf8aa09e9ada41a222546fea9182b
ABHI <ABHI@LAPTOP-399HN2CI>
2020-02-23 01:00:25 +0530

    9652

---
+++ src/main/java/in/nit/model/Student.java
@@ -0,0 +1,18 @@
+package in.nit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(generator = "increment")
+	private Integer id;
+	private String name;
+	private String sfee;
+
+}
